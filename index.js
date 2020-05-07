5bbb3f1855e345cf11c63e95117a8da5c95329eb
jiggag <jiggag90@gmail.com>
2020-05-07 23:42:32 +0900

    feat: sentry

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
+import { init as SentryInit } from '@sentry/react-native';
 import App from './src';
 import { name as appName } from './app.json';
 import { SENTRY_DSN } from './utils/Constants';
@@ -6,7 +7,9 @@ import { SENTRY_DSN } from './utils/Constants';
 require('react-native').unstable_enableLogBox();
 
 if (SENTRY_DSN) {
-  // TODO sentry error tracking
+  SentryInit({
+    dsn: SENTRY_DSN,
+  });
 }
 
 AppRegistry.registerComponent(appName, () => App);
