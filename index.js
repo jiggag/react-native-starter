10efad5b0ad1af3369faab5aadefdb7ec10639c0
jiggag <jiggag90@gmail.com>
2020-07-15 23:31:18 +0900

    refactor: RN 0.63 내장된 log box 코드 제거

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ import App from './src';
 import { name as appName } from './app.json';
 import { SENTRY_DSN } from './utils/Constants';
 
-require('react-native').unstable_enableLogBox();
-
 if (SENTRY_DSN) {
   SentryInit({
     dsn: SENTRY_DSN,
