515f4f7a6efb416d300b4399cfad0b4caf8c8f04
jiggag <jiggag90@gmail.com>
2021-06-23 23:40:20 +0900

    feat: build.gradle.kts

---
+++ android/buildSrc/src/main/java/com/jiggag/rnstarter/Constants.kt
@@ -0,0 +1,13 @@
+package com.jiggag.rnstarter
+
+object Constants {
+    const val KOTLIN_VERSION = "1.4.32"
+    const val BUILD_TOOLS_VERSION = "29.0.3"
+    const val MIN_SDK_VERSION = 21
+    const val COMPILE_SDK_VERSION = 29
+    const val TARGET_SDK_VERSION = 29
+    const val NDK_VERSION = "20.1.5948944"
+    const val BUGSNAG_API_KEY = "TODO"
+    const val CODE_PUSH_AOS_KEY = ""
+    const val FLIPPER_VERSION = "0.75.1"
+}
