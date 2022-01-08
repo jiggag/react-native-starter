ce82dda74eceb47bff7da85e572b9fd4feb468bb
jiggag <jiggag90@gmail.com>
2022-01-09 00:13:51 +0900

    feat: android java -> kotlin

---
+++ android/app/src/main/java/com/jiggag/rnstarter/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.jiggag.rnstarter
+
+import com.facebook.react.ReactActivity
+
+class MainActivity : ReactActivity() {
+
+  override fun getMainComponentName(): String {
+    return "RNStarter"
+  }
+}
