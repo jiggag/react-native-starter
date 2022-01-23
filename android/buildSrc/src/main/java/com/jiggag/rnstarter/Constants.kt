d97fca6965293216677322d4602af58f3cb8bccf
jiggag <jiggag90@gmail.com>
2022-01-23 18:09:03 +0900

    fix: gradle.properties에서 flipper-version 가져오도록 수정

--- android/buildSrc/src/main/java/com/jiggag/rnstarter/Constants.kt
+++ android/buildSrc/src/main/java/com/jiggag/rnstarter/Constants.kt
@@ -1,5 +1,15 @@
 package com.jiggag.rnstarter
 
+import java.io.FileInputStream
+import java.util.Properties
+
+private fun getProperty(key: String): String {
+    val properties = Properties()
+    properties.load(FileInputStream("gradle.properties"))
+
+    return properties.getProperty(key)
+}
+
 object Constants {
     const val KOTLIN_VERSION = "1.4.32"
     const val BUILD_TOOLS_VERSION = "30.0.2"
@@ -7,7 +17,7 @@ object Constants {
     const val COMPILE_SDK_VERSION = 31
     const val TARGET_SDK_VERSION = 31
     const val NDK_VERSION = "21.4.7075529"
-    const val FLIPPER_VERSION = "0.128.4"
+    val FLIPPER_VERSION = getProperty("FLIPPER_VERSION")
     const val VERSION_CODE = 1
     const val VERSION_NAME = "0.0.1"
 }
