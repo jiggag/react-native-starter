76e2fb32b593edc6180258d5607b3bb2ef03c039
jiggag <jiggag90@gmail.com>
2022-01-31 15:03:27 +0900

    test: dynamic import plugin

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    'dynamic-import-node',
     [
       'module-resolver',
       {
--- jest.config.js
+++ jest.config.js
@@ -6,14 +6,15 @@ module.exports = {
     'js',
     'jsx',
   ],
-  setupFiles: [
-    '<rootDir>/__tests__/setup.ts',
-  ],
+  setupFiles: [],
   globals: {
     window: {}
   },
   transformIgnorePatterns: [
     'node_modules/(?!@react-native|react-native)'
   ],
+  moduleNameMapper: {
+    'react-query-native-devtools': '<rootDir>/__mocks__/react-query-native-devtools.ts'
+  },
   testRegex: '__tests__/.*\\.test\\.(tsx|ts)$'
 };
