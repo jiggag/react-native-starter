5630b74f956410ff7757796ca4e4dd18d635a842
jiggag <jiggag90@gmail.com>
2022-02-27 23:53:13 +0900

    test: notifee mock

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import { App } from '../src/App';
 
+jest.mock('@notifee/react-native', () => ({
+  requestPermission: jest.fn(),
+  onForegroundEvent: jest.fn(),
+}));
+
 it('renders correctly', () => {
   const { toJSON } = render(<App />);
   expect(toJSON()).toMatchSnapshot();
