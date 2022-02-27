64c38804a9883b39bdf2caa8e6bccc422f6f4faf
jiggag <jiggag90@gmail.com>
2022-02-27 21:52:24 +0900

    feat: notifee 테스트 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
+import notifee, { EventType, IOSAuthorizationStatus } from '@notifee/react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -21,6 +22,32 @@ if (__DEV__) {
 const Stack = createNativeStackNavigator();
 
 export function App() {
+  const checkApplicationPermission = useCallback(async () => {
+    const settings = await notifee.requestPermission();
+    if (settings.authorizationStatus >= IOSAuthorizationStatus.AUTHORIZED) {
+      console.log('Permission settings:', settings);
+    } else {
+      console.log('User declined permissions');
+    }
+  }, []);
+
+  useEffect(() => {
+    checkApplicationPermission();
+
+    return notifee.onForegroundEvent(({ type, detail }) => {
+      switch (type) {
+        case EventType.DISMISSED:
+          console.log('User dismissed notification', detail.notification);
+          break;
+        case EventType.PRESS:
+          console.log('User pressed notification', detail.notification);
+          break;
+        default:
+          break;
+      }
+    });
+  }, [checkApplicationPermission]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
--- src/screens/Detail.tsx
+++ src/screens/Detail.tsx
@@ -1,7 +1,8 @@
 import React, { useCallback } from 'react';
 import {
-  Text, SafeAreaView, ScrollView, StatusBar, StyleSheet, useColorScheme, Button,
+  Button, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme,
 } from 'react-native';
+import notifee, { RepeatFrequency, TimestampTrigger, TriggerType } from '@notifee/react-native';
 import { useNavigation } from '@react-navigation/native';
 
 export function Detail() {
@@ -12,12 +13,53 @@ export function Detail() {
     navigation.goBack();
   }, [navigation]);
 
+  const onDisplayNotification = useCallback(async () => {
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    await notifee.displayNotification({
+      title: '알림',
+      body: '내용',
+      android: {
+        channelId,
+      },
+    });
+  }, []);
+
+  const onCreateTriggerNotification = useCallback(async () => {
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    const trigger: TimestampTrigger = {
+      type: TriggerType.TIMESTAMP,
+      timestamp: Date.now() + 1000 * 10,
+      repeatFrequency: RepeatFrequency.HOURLY,
+    };
+
+    await notifee.createTriggerNotification(
+      {
+        title: '알림',
+        body: '10초 후',
+        android: {
+          channelId,
+        },
+      },
+      trigger,
+    );
+  }, []);
+
   return (
     <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <Text style={styles.sectionTitle}>Starter (Detail)</Text>
         <Button title="Go back" onPress={onPress} />
+        <Button title="Display Notification" onPress={onDisplayNotification} />
+        <Button title="Trigger Notification" onPress={onCreateTriggerNotification} />
       </ScrollView>
     </SafeAreaView>
   );
