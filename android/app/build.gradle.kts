b5f248605875cb2094ee4c8cf56cdea6dc980456
jiggag <jiggag90@gmail.com>
2022-11-05 21:19:07 +0900

    jniLibs *.so 빌드 오류 임시 대응
    - RNGP 참고

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,13 +32,11 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 
+    // TODO 임시 대응
     packagingOptions {
-        resources.pickFirsts.addAll(
+        jniLibs.pickFirsts.addAll(
             listOf(
-                "lib/x86/libc++_shared.so",
-                "lib/x86_64/libc++_shared.so",
-                "lib/armeabi-v7a/libc++_shared.so",
-                "lib/arm64-v8a/libc++_shared.so"
+                "**/*.so"
             )
         )
     }
