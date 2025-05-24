dc2bdbee67222d2de034c7c6296e964bbc99c60b
jiggag <jiggag90@gmail.com>
2025-05-24 11:41:47 +0900

    react-native@0.79.2

--- android/buildSrc/src/main/java/com/jiggag/rnstarter/Constants.kt
+++ android/buildSrc/src/main/java/com/jiggag/rnstarter/Constants.kt
@@ -17,7 +17,7 @@ object Constants {
     const val BUILD_TOOLS_VERSION = "35.0.0"
     const val MIN_SDK_VERSION = 24
     const val COMPILE_SDK_VERSION = 35
-    const val TARGET_SDK_VERSION = 34
+    const val TARGET_SDK_VERSION = 35
     val NDK_VERSION = "27.1.12297006"
     val REACT_NATIVE_ARCHITECTURES = getProperty("reactNativeArchitectures")
     val HERMES_ENABLED = getProperty("hermesEnabled")
